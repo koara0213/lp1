7b1f6b0f6f5bd6f4598137c784b285ca094c9b2e
koara0213 <mayu.koakoa.pearl@gmail.com>
2024-09-03 23:37:46 +0900

    キラキラ塾初稿

---
+++ assets/script/main.js
@@ -0,0 +1,56 @@
+// ハンバーガーメニュー
+var hamburger = $('.hamburger-menu');
+
+$('.hamburger-button').on('click', function () {
+  hamburger.toggleClass('hamburger-menu-active');
+});
+
+$('.hamburger-menu-list').on('click', function () {
+  hamburger.removeClass('hamburger-menu-active');
+});
+
+$(window).on('resize', function () {
+  hamburger.removeClass('hamburger-menu-active');
+});
+
+// リンクを滑らかにスクロール
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+  });
+});
+
+// コース別・目的別
+  document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.getAttribute('data-target');
+
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+        document.getElementById(target).classList.add('active');
+    });
+});
+
+// FAQ
+$(document).ready(function () {
+  $('.faq-bg-wrapper').hover(
+    function () {
+      const hoverImage = $(this).data('hover');
+      $(this).css('background-image', `url(${hoverImage})`);
+      $(this).css('transform', 'scale(1.2)');
+    },
+    function () {
+      const originalImage = $(this).css('background-image').replace('-1', '');
+      $(this).css('background-image', `${originalImage}`);
+      $(this).css('transform', 'scale(1)');
+    }
+  );
+});
